f136bc1d18e403c42c8b321db6a6b3d295224994
zeeshanvirani <zvirani99@gmail.com>
2022-11-17 11:59:08 -0600

    added check for already brewing, added permission check for bluetooth, added error message handling

--- Zeeshan/app/src/main/java/com/zeeshanvirani/projectcelia/BrewingProcess.java
+++ Zeeshan/app/src/main/java/com/zeeshanvirani/projectcelia/BrewingProcess.java
@@ -1,7 +1,7 @@
 package com.zeeshanvirani.projectcelia;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
@@ -11,27 +11,31 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
 
 public class BrewingProcess extends AppCompatActivity {
 
     public static final String TAG = "ProjectCelia:BrewingProcess";
+
+    public static final String TAG_FIREBASE_ID = "firebaseid";
     public static final String TAG_TEMPERATURE = "temperature";
     public static final String TAG_TARGET_SATURATION = "saturation";
 
     private BluetoothAdapter btAdapter;
     private BluetoothSocket btSocket;
 
-    private String temperature, target_saturation;
-
-    public MyBroadcastReceiver myReceiver;
+    private String temperature, target_saturation, firebaseid;
 
     TextView brewing_text;
     MaterialButton returnhome_btn;
@@ -46,6 +50,7 @@ public class BrewingProcess extends AppCompatActivity {
         Intent i = getIntent();
         temperature = i.getStringExtra(TAG_TEMPERATURE);
         target_saturation = i.getStringExtra(TAG_TARGET_SATURATION);
+        firebaseid = i.getStringExtra(TAG_FIREBASE_ID);
 
         // Initialize views
         brewing_text = findViewById(R.id.brewing_text);
@@ -53,52 +58,85 @@ public class BrewingProcess extends AppCompatActivity {
         continuebrewing_btn = findViewById(R.id.brewing_continuebrewing);
 
         // End activity on return button clicked
-        returnhome_btn.setOnClickListener(view -> finish());
-        continuebrewing_btn.setOnClickListener(view -> continueBrewingClicked() );
-
-        // Setup broadcast Receivers
-        myReceiver = new MyBroadcastReceiver(this);
+        returnhome_btn.setOnClickListener(view -> {
+            finish();
+        });
+        continuebrewing_btn.setOnClickListener(view -> continueBrewingClicked());
 
         // Check if device is already connected, if not register broadcast receiver for bluetooth
         if (!DataHandler.DEVICE_CONNECTED) {
+            Log.d(TAG, "No Device connected. Registering receiver.");
             IntentFilter filter = new IntentFilter();
             filter.addAction(BluetoothDevice.ACTION_FOUND);
             filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
             filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-            myReceiver.register(this, filter); // register
+            this.registerReceiver(myBroadcastReceiver, filter);
             startPairing();
-        }
+        } else
+            Log.d(TAG, "Device is already connected. Receiver NOT registered.");
 
         // Waits until device is connected, then beginBrewing is called from establishHandler()
+    }
 
+    @Override
+    public void onDestroy() {
+        this.unregisterReceiver(myBroadcastReceiver);
+        if (DataHandler.DEVICE_CONNECTED) {
+            DataHandler.btSocketHandler.closeSocket();
+        }
+        super.onDestroy();
     }
 
     // Called when messages received from bluetooth input stream
-    public void statusUpdate( String newStatus ) {
+    public void statusUpdate(String newStatus) {
         switch (newStatus) {
+            case "CONNECTED":
+                brewing_text.setText("Connected to device.");
+                break;
+            case "UNABLE_TO_CONNECT":
+                brewing_text.setText("Unable to connect to device.");
+                returnhome_btn.setVisibility(View.VISIBLE);
+                break;
             case "OUT_OF_WATER":
                 brewing_text.setText("The reservoir is out of water.");
-                DataHandler.sendNotification( this, "Out of Water", "The water tank is out of water. Please refill and hit continue to resume brewing.", 1234);
+                DataHandler.sendNotification(this, "Out of Water", "The water tank is out of water. Please refill and hit continue to resume brewing.", 1234, DataHandler.NOTIFICATION_TYPE.BREWSTATUS);
                 continuebrewing_btn.setVisibility(View.VISIBLE);
-                return;
-            case "UNKNOWN_ERROR":
-                return;
+                break;
             case "STARTED_HEATING":
                 brewing_text.setText("Heating water.");
-                return;
+                break;
             case "STARTED_POURING":
                 brewing_text.setText("Pouring water.");
-                return;
+                break;
             case "BREWING_COMPLETE":
                 brewing_text.setText("Your coffee is ready!");
-                DataHandler.sendNotification( this, "Your coffee is ready!", "", 1234);
+                DataHandler.sendNotification(this, "Your coffee is ready!", "", 1234, DataHandler.NOTIFICATION_TYPE.BREWSTATUS);
                 // DISCONNECT BLUETOOTH DEVICE
                 DataHandler.btSocketHandler.closeSocket();
                 DataHandler.btSocketHandler = null;
+                DataHandler.updateIsBrewing(this, false);
+
+                if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    DocumentReference docRef = FirebaseFirestore.getInstance().collection("brews")
+                            .document( firebaseid );
+                    docRef.update(DataHandler.DB_STATUS, DataHandler.DB_STATUS_DONE);
+                    docRef.get().addOnSuccessListener(documentSnapshot -> {
+                        Log.d(TAG, "Updated status of brew on database.");
+                    });
+                }
+
+                returnhome_btn.setVisibility(View.VISIBLE);
+                break;
+            default:
+                brewing_text.setText(newStatus);
+                DataHandler.sendNotification(this, "An error has occurred", newStatus, 1234, DataHandler.NOTIFICATION_TYPE.BREWSTATUS);
+                // DISCONNECT BLUETOOTH DEVICE
+                DataHandler.btSocketHandler.closeSocket();
+                DataHandler.btSocketHandler = null;
+                DataHandler.updateIsBrewing(this, false);
                 returnhome_btn.setVisibility(View.VISIBLE);
                 break;
         }
-
     }
 
     // Start bluetooth pairing process
@@ -116,9 +154,23 @@ public class BrewingProcess extends AppCompatActivity {
                 Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivity(turnOn);
             }
-            // Check if bluetooth is already in discoverable mode, if yes: cancel discovery
-            if ( btAdapter.isDiscovering() ) btAdapter.cancelDiscovery();
-            Log.d(TAG, String.valueOf(btAdapter.startDiscovery()));
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
+                    if (!isGranted.containsValue(false)) {
+                        // Check if bluetooth is already in discoverable mode, if yes: cancel discovery
+                        if (btAdapter.isDiscovering()) btAdapter.cancelDiscovery();
+                        Log.d(TAG, String.valueOf(btAdapter.startDiscovery()));
+                    } else {
+                        Log.d(TAG, "Bluetooth permission not given.");
+                    }
+                }).launch(new String[]{Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT});
+            } else {
+                // Check if bluetooth is already in discoverable mode, if yes: cancel discovery
+                if (btAdapter.isDiscovering()) btAdapter.cancelDiscovery();
+                Log.d(TAG, String.valueOf(btAdapter.startDiscovery()));
+            }
+
         } catch (SecurityException e ) {
             e.printStackTrace();
         }
@@ -145,59 +197,31 @@ public class BrewingProcess extends AppCompatActivity {
         DataHandler.btSocketHandler.sendMessage("CONTINUE_BREWING");
         // Status update will come from Pi
     }
-}
 
-class MyBroadcastReceiver extends BroadcastReceiver {
-
-    public boolean isRegistered;
-    private final BrewingProcess activity;
-
-    private boolean deviceFound = false;
-
-    public MyBroadcastReceiver(BrewingProcess activity) {
-        this.activity = activity;
-    }
-
-    // Registers the broadcast receiver with the system
-    public void register(Context context, IntentFilter filter) {
-        try {
-            Log.d(BrewingProcess.TAG, "Registered Receiver Adapter");
-            context.registerReceiver(this, filter);
-        } finally {
-            isRegistered = true;
-        }
-    }
-
-    // Unregisters the receiver from the system
-    public void unregister(Context context) {
-        Log.d(BrewingProcess.TAG, "Unregistered Receiver Adapter");
-        context.unregisterReceiver(this);
-        isRegistered = false;
-    }
-
-    // Called for any event the receiver is filtered for
-    public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-            //discovery starts, we can show progress dialog or perform other tasks
-            Log.d(BrewingProcess.TAG, "Started Bluetooth Discovery");
-        } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-            //discovery finishes, dismiss progress dialog
-            if ( !deviceFound ) {
-                Log.d(BrewingProcess.TAG, "Device not found.");
-                activity.brewing_text.setText( R.string.brewingprocess_cannotfinddevice );
-                activity.returnhome_btn.setVisibility( View.VISIBLE );
+    private final BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
+        boolean deviceFound = false;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                //discovery starts, we can show progress dialog or perform other tasks
+                Log.d(BrewingProcess.TAG, "Started Bluetooth Discovery");
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //discovery finishes, dismiss progress dialog
+                if ( !deviceFound ) {
+                    Log.d(BrewingProcess.TAG, "Device not found.");
+                    brewing_text.setText( R.string.brewingprocess_cannotfinddevice );
+                    returnhome_btn.setVisibility( View.VISIBLE );
+                }
+                Log.d(BrewingProcess.TAG, "Discovery Completed");
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if ( device.getAddress().equals( DataHandler.DEVICE_MACADDR ) ) {
+                    deviceFound = true;
+                    establishHandler(device);
+                }
             }
-            Log.d(BrewingProcess.TAG, "Discovery Completed");
-            unregister( context );
-        } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-            //bluetooth device found
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-            if ( device.getAddress().equals( DataHandler.DEVICE_MACADDR ) ) {
-                deviceFound = true;
-                activity.establishHandler(device);
-            }
-
         }
-    }
+    };
 }
